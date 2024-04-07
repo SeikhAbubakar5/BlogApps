20fbfc3c5bc4a28fb4c9c9c3fcc52d49054ca281
SeikhAbubakar5 <seikhabubakar47@gmail.com>
2024-04-07 17:26:10 +0530

    cors update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 require('dotenv').config({path:'src/.env'})
 const express= require("express")
 const mongoose=require("mongoose")
+const cors=require("cors")
 
 const userRoutes=require("./routes/userRouter")
 const blogRoutes=require("./routes/blogRoutes")
 const commentRoutes=require("./routes/commentRoute")
 const app= express();
 
-
+app.use(cors())
 app.use(express.json())
 app.use("/api/user",userRoutes )
 app.use("/api/blog",blogRoutes)
@@ -16,7 +17,7 @@ app.use("/api/comment",commentRoutes)
 const PORT=process.env.PORT || 5000;
 
 
-mongoose.connect(process.env.MONGO_URI,{
+mongoose.connect(process.env.MONGODB_URI,{
 }).then(()=>{
     console.log("MongoDB connected")
 }).catch((err)=>{
