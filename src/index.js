f945b8d5fd8daf50b52991a732bf8156b65d8dfc
SeikhAbubakar5 <128287400+SeikhAbubakar5@users.noreply.github.com>
2024-04-07 15:46:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use("/api/comment",commentRoutes)
 const PORT=process.env.PORT || 5000;
 
 
-mongoose.connect(process.env.MONGO_URI,{
+mongoose.connect(process.env.MONGODB_URI,{
 }).then(()=>{
     console.log("MongoDB connected")
 }).catch((err)=>{
